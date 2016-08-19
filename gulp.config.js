dc0ef949887e31be783f31606f830c3a43c42592
Alper Dortbudak <adortbudak@outlook.com>
2016-08-19 12:43:29 -0500

    project plumbing

---
+++ gulp.config.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+    var config = {
+        alljs: ['./*.js',
+                './src/client/**/*.js'
+            ]
+    };
+
+    return config;
+};
---
+++ main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by adortbud on 8/19/2016.
+ */
+const electron = require('electron');
+const app = electron.app;
+
+const BrowserWindow  = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on('ready',function () {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        resizable: false
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/src/client/index.html');
+
+    mainWindow.openDevTools();
+});
+
+
